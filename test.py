5d4359e187a1a26c4ea2aeab1705a9de91b6ac79
Benjamín Mario Sainz-Tinajero <bm.sainz@gmail.com>
2021-06-29 17:53:13 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 """
-Created on Fri Mar 19 14:09:49 2021
-
-@author: benjamin
+Authors: Katti Faceli, Marcilio C.P. de Suoto, Daniel S.A. de Araújo, and André C.P.L.F. de Carvalho.
+Code: Benjamin Mario Sainz-Tinajero
+Year: 2021.
+https://github.com/benjaminsainz/mocle
 """
 
 from retr import *
