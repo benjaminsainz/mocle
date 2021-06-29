bc30c638ab6b74c8d1563acbdddd51cca06cae50
Benjamín Mario Sainz-Tinajero <bm.sainz@gmail.com>
2021-06-29 17:50:23 -0500

    Update ind.py

--- ind.py
+++ ind.py
@@ -1,8 +1,10 @@
 """
-Created on Thu Mar 18 10:29:01 2021
-
-@author: benjamin
+Authors: Katti Faceli, Marcilio C.P. de Suoto, Daniel S.A. de Araújo, and André C.P.L.F. de Carvalho.
+Code: Benjamin Mario Sainz-Tinajero
+Year: 2021.
+https://github.com/benjaminsainz/mocle
 """
+
 from sklearn.cluster import KMeans
 from sklearn.cluster import AgglomerativeClustering
 from snn import *
