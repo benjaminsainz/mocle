4c9868cda9783620c67293e25a101621773c0991
Benjamin M. Sainz-Tinajero <bm.sainz@gmail.com>
2022-05-07 12:16:40 -0500

    Update and rename test.py to excample.py

---
+++ excample.py
@@ -0,0 +1,23 @@
+"""
+Author: Benjamin M. Sainz-Tinajero @ Tecnologico de Monterrey, 2022.
+
+"""
+
+
+from retr import *
+from gen import *
+
+
+ds = ['absenteeism-at-work', 'arrhythmia', 'breast-cancer-wisconsin', 'breast-tissue', 'car-evaluation', 'dermatology',
+      'echocardiogram', 'ecoli', 'forest', 'forest-fires', 'german-credit', 'glass', 'hepatitis', 'image-segmentation',
+      'ionosphere', 'iris', 'leaf', 'liver', 'parkinsons', 'seeds', 'segment', 'sonar', 'soybean-large',
+      'student-performance', 'tic-tac-toe', 'transfusion', 'user-knowledge-modeling', 'wine', 'yeast', 'zoo']
+    
+      
+nature = ['canada', 'coast', 'highway-in-the-desert', 'london', 'parking-lot', 'port-city', 'port', 'road-with-trees', 
+          'varadero', 'white-containers']
+        
+if __name__ == "__main__":
+    for d in ds:
+        run_mocle(d, 'auto', runs=10, max_gens=50)
+    
