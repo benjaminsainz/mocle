01e72c04c57acca66095486a69002ba33f68fe7f
Benjamin M. Sainz-Tinajero <bm.sainz@gmail.com>
2022-05-07 12:17:30 -0500

    Update ind.py

--- ind.py
+++ ind.py
@@ -1,12 +1,14 @@
 """
-Authors: Katti Faceli, Marcilio C.P. de Suoto, Daniel S.A. de Araújo, and André C.P.L.F. de Carvalho.
-Code: Benjamin Mario Sainz-Tinajero
-Year: 2021.
-https://github.com/benjaminsainz/mocle
+Author: Benjamin M. Sainz-Tinajero @ Tecnologico de Monterrey, 2022.
+
 """
+
 
 from sklearn.cluster import KMeans
 from sklearn.cluster import AgglomerativeClustering
+
+import numpy as np
+
 from snn import *
 
 
@@ -14,62 +16,58 @@ def takesecond(elem):
     return elem[1]
 
 
-def initial_pop(X, k, k_range, representation):
-    pop = list()
-    nn = [int(.2*len(X)), int(.5*len(X)), int(.1*len(X)), int(.2*len(X)), int(.3*len(X)), int(.4*len(X))]
-    min_shared = [0, 0.2, 0.4, 0.5, 0.6, 0.8, 1]
-    if k_range is True:
-        kmin = min(list(k))
-        kmax = max(list(k))
-        for n in k:
-            temp = []
-            for _ in range(30):
-                km_model = KMeans(n, init='random').fit(X)  # k-means model
-                temp.append((km_model.labels_, km_model.inertia_))
-            temp.sort(key=takesecond)
-            pop.append(temp[0][0])
-            al_model = AgglomerativeClustering(n, linkage='average', affinity='euclidean').fit(X)  # average link model
-            pop.append(al_model.labels_)
-            sl_model = AgglomerativeClustering(n, linkage='single', affinity='euclidean').fit(X)  # single link model
-            pop.append(sl_model.labels_)
-        for i in nn:
-            for j in min_shared:
-                snn_model = SNN(neighbor_num=i, min_shared_neighbor_proportion=j).fit(X)  # snn model
-                if (len(set(snn_model.labels_)) >= kmin) and len(set(snn_model.labels_)) <= kmax:
-                    pop.append(snn_model.labels_)
-    else:
-        for _ in range(k, k*2+1):
-            temp = list()
-            for _ in range(30):
-                km_model = KMeans(k, init='random').fit(X)  # k-means model
-                temp.append((km_model.labels_, km_model.inertia_))
-            temp.sort(key=takesecond)
-            pop.append(temp[0][0])
-            al_model = AgglomerativeClustering(k, linkage='average', affinity='euclidean').fit(X)  # average link model
-            pop.append(al_model.labels_)
-            sl_model = AgglomerativeClustering(k, linkage='single', affinity='euclidean').fit(X)  # single link model
-            pop.append(sl_model.labels_)
-        for i in nn:
-            for j in min_shared:
-                snn_model = SNN(neighbor_num=i, min_shared_neighbor_proportion=j).fit(X)  # snn model
-                if len(set(snn_model.labels_)) == k:
-                    pop.append(snn_model.labels_)
+def km_agg_cluster(args):
+    n, X = args
+    pop = []
+    km_model = KMeans(n, init='random', n_init=30).fit(X)  # k-means model
+    pop.append(km_model.labels_)
+    al_model = AgglomerativeClustering(n, linkage='average', affinity='euclidean').fit(X)  # average link model
+    pop.append(al_model.labels_)
+    sl_model = AgglomerativeClustering(n, linkage='single', affinity='euclidean').fit(X)  # single link model
+    pop.append(sl_model.labels_)
+    return pop
 
-    temp_ind = list()
-    if representation == 'sets':
-        temp_pop = list()
-        for individual in pop:
-            d = dict()
-            for gene in range(len(individual)):
-                d['{}'.format(individual[gene])] = []
-            for gene in range(len(individual)):
-                for key in d.keys():
-                    if individual[gene] == int(key):
-                        d[key].append(gene)
-            temp_ind = list()
-            for c in d.values():
-                temp_ind.append(set(c))
-            temp_pop.append(np.array(temp_ind))
-        return temp_ind
-    elif representation == 'label':
-        return pop
+
+def snn_cluster(args):
+    i, j, X, kmin, kmax = args
+    pop = []
+    snn_model = SNN(neighbor_num=i, min_shared_neighbor_proportion=j).fit(np.array(X))  # snn model
+    if (len(set(snn_model.labels_)) >= kmin) and len(set(snn_model.labels_)) <= kmax:
+        pop.append(snn_model.labels_ + 1)
+    return pop
+
+
+def avoid_singleton_clusters(pop):
+    no_singletons_pop = []
+    for ind in pop:
+        ind = list(ind)
+        k_values_ind = set(ind)
+        singleton_flag = False
+        for j in k_values_ind:
+            if ind.count(j) == 1:
+                singleton_flag == True
+        if singleton_flag == False:
+            no_singletons_pop.append(ind)
+    return no_singletons_pop
+
+
+def create_pop_with_k(n_clusters, X, pool):
+    kmin = n_clusters
+    kmax = 2*n_clusters
+    k_values = list(range(kmin, kmax+1))
+    km_agg_args = [[k, X] for k in k_values]
+    parallel_km_agg_pop = list(pool.map(km_agg_cluster, km_agg_args))
+    km_agg_pop = [ind for n_pop in parallel_km_agg_pop for ind in n_pop]
+    snn_args = [[int(.01*len(X)), 0.5, X, kmin, kmax]]
+    snn_parallel_pop = list(pool.map(snn_cluster, snn_args))
+    snn_pop = [ind for n_pop in snn_parallel_pop for ind in n_pop]
+    pop = km_agg_pop + snn_pop
+    no_singletons_pop = avoid_singleton_clusters(pop)
+    return no_singletons_pop, k_values
+
+
+def initial_pop(X, n_clusters, pool, max_gens):
+    print('Generating initial population...')
+    pop, k = create_pop_with_k(n_clusters, X, pool)
+    print('Population size: {}. Generations: {}'.format(len(pop), max_gens))
+    return pop, k, len(pop)
