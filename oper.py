35fa7ce95580303b9b29901cce1e20737ebce2e6
Benjamin M. Sainz-Tinajero <bm.sainz@gmail.com>
2022-05-07 12:18:24 -0500

    Update oper.py

--- oper.py
+++ oper.py
@@ -1,36 +1,46 @@
 """
-Authors: Katti Faceli, Marcilio C.P. de Suoto, Daniel S.A. de Araújo, and André C.P.L.F. de Carvalho.
-Code: Benjamin Mario Sainz-Tinajero
-Year: 2021.
-https://github.com/benjaminsainz/mocle
+Author: Benjamin M. Sainz-Tinajero @ Tecnologico de Monterrey, 2022.
+
 """
+
 
 from mcla import *
 from obj import *
+
+from sklearn.metrics.cluster import adjusted_rand_score
+
+
+def compute_ari(arguments):
+    x, y = arguments
+    return adjusted_rand_score(x, y)
 
 
 def binary_tournament(X, pop, f1, f2):
     pop_size = len(pop)
     i, j = np.random.randint(pop_size), np.random.randint(pop_size)
-    """
-    while j == i:
-        j = np.random.randint(pop_size)
-    if ((conn(X, pop[i]) <= conn(X, pop[j])) and (dev(X, pop[i]) <= dev(X, pop[j]))) \
-            and ((conn(X, pop[i]) < conn(X, pop[j])) or (dev(X, pop[i]) < dev(X, pop[j]))):
-        return pop[i]
-    else:
-        return pop[j]
-    """
     if (f2[i] <= f2[j]) and (f1[i] <= f1[j]) and (f2[i] < f2[j]) or (f1[i] < f1[j]):
         return pop[i]
     else:
         return pop[j]
 
 
-def cross(p1, p2, k, k_range):
-    if k_range is True:
-        n = np.random.randint(min(list(k)), max(list(k)))
-    else:
-        n = k
-    offspring = MCLA(np.array([p1, p2]), n)
+def cross(p1, p2, k_values):
+    p1 = list(p1)
+    p2 = list(p2)
+    p1_n_clusters = len(set(p1))
+    p2_n_clusters = len(set(p2))
+    parent_ks = [p1_n_clusters, p2_n_clusters]
+    parent_range = list(range(min(parent_ks), max(parent_ks)+1, 1))
+    N_clusters_max = parent_range[np.random.randint(len(parent_range))]
+    offspring = MCLA(np.array([p1, p2]), verbose = 0, N_clusters_max = N_clusters_max)
     return offspring
+
+
+def select_and_recombine(X, pop, function1_values, function2_values, k_values, temp_pop, pool):
+    p1 = binary_tournament(X, pop, function1_values, function2_values)
+    p2 = binary_tournament(X, pop, function1_values, function2_values)
+    offspring = cross(p1, p2, k_values)
+    ari_arguments = [[ind, offspring] for ind in temp_pop]
+    rand_indexes_offspring_temp_pop = list(pool.map(compute_ari, ari_arguments))
+    offspring_in_temp_pop = max(rand_indexes_offspring_temp_pop)
+    return offspring, offspring_in_temp_pop
